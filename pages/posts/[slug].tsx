d99121df04ee937070a3105b20ad9048cb3c1fb5
Dalmo Mendonça <dalmo3@gmail.com>
2020-10-14 17:38:41 +1300

    trying with next/dynamic but bugged

--- pages/posts/[slug].tsx
+++ pages/posts/[slug].tsx
@@ -9,17 +9,29 @@ import Head from 'next/head';
 import renderToString from 'next-mdx-remote/render-to-string';
 import hydrate from 'next-mdx-remote/hydrate';
 interface PostProps {
+  compDesc: string[][];
   source: any;
   postData: PostData;
 }
-
+import dynamic from 'next/dynamic'
 import Link from 'next/link';
 import { FC } from 'react';
-const components = {
-  Link
-};
+// const components = {
+//   Link
+// };
+
+const getComponents = (path) => ({
+  Link: dynamic(()=>import(path))
+})
 
 const Post: FC<PostProps> = ({ source, postData }) => {
+  // const Link = getComponents('next/link').Link
+  
+  const components = {
+    // Link: dynamic(()=>import('next/link'))
+    Link
+  }
+  // console.log('comps', getComponents('next/link'))
   const content = hydrate(source, { components });
 
   return (
@@ -46,11 +58,20 @@ export async function getStaticProps({
   params
 }: PostParams): Promise<{ props: PostProps }> {
   const postData = await getPostData(params.slug);
+  const compDesc = [['Link','../../node_modules/next/link']]
+  
+  const components = {
+  }
+  compDesc.forEach(desc => {
+    components[desc[0]] = dynamic(()=>import(desc[1]))
+  })
+  console.log(components)
   const source = await renderToString(postData.content, {
     components
   });
   return {
     props: {
+      compDesc,
       source,
       postData
     }
