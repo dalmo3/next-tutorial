a45e00bfa99e522381366748bbee5f22c2100440
Dalmo Mendonça <dalmo3@gmail.com>
2020-10-17 14:57:31 +1300

    added grommet header

--- components/SiteHeader.tsx
+++ components/SiteHeader.tsx
@@ -1,6 +1,34 @@
+import { Box, Button, Heading, ResponsiveContext } from 'grommet';
+import { Notification } from 'grommet-icons';
 import Link from 'next/link';
-import React, { FC } from 'react';
+import React, { FC, useContext } from 'react';
 
-export const SiteHeader: FC = () => <Link href="/">Home</Link>;
+export const SiteHeader: FC = (props) => {
+  const responsive = useContext(ResponsiveContext);
+  console.log(responsive);
+
+  return (
+    <Box
+      tag="header"
+      direction="row"
+      align="center"
+      justify="between"
+      background="brand"
+      pad={{ left: 'medium', right: 'small', vertical: 'small' }}
+      elevation="medium"
+      {...props}
+    >
+      <Box direction="row">
+        <Heading level="1" margin="none">
+          My Blog
+        </Heading>
+        <Heading level="1" margin="none">
+          My Blog
+        </Heading>
+      </Box>
+      <Button icon={<Notification />} />
+    </Box>
+  );
+};
 
 export default SiteHeader;
--- components/SiteLayout.tsx
+++ components/SiteLayout.tsx
@@ -1,13 +1,27 @@
+import { Grommet } from 'grommet';
 import React, { FC } from 'react';
 import SiteFooter from './SiteFooter';
 import SiteHeader from './SiteHeader';
 
+const theme = {
+  global: {
+    font: {
+      // family: 'Fira Code',
+      size: '18px',
+      height: '20px'
+    },
+    colors:{
+      brand: '#303030'
+    } 
+  }
+};
+
 export default function SiteLayout({ children }) {
   return (
-    <>
-      <SiteHeader />
+    <Grommet theme={theme}>
+      <SiteHeader/>
       {children}
       <SiteFooter />
-    </>
+    </Grommet>
   );
 }
