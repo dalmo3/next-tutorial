ad2a8dce5ad3f085a8039f8d1026bafd84000719
Dalmo Mendonça <dalmo3@gmail.com>
2020-10-19 12:35:06 +1300

    add theme-ui

--- components/SiteLayout.tsx
+++ components/SiteLayout.tsx
@@ -1,13 +1,15 @@
 import React, { FC } from 'react';
 import SiteFooter from './SiteFooter';
 import SiteHeader from './SiteHeader';
+import { ThemeProvider } from 'theme-ui'
+import theme from 'styles/theme'
 
 export default function SiteLayout({ children }) {
   return (
-    <>
+    <ThemeProvider theme={theme}>
       <SiteHeader />
       {children}
       <SiteFooter />
-    </>
+    </ThemeProvider>
   );
 }
---
+++ styles/theme.ts
@@ -0,0 +1,13 @@
+import { Theme } from "theme-ui"
+
+const theme : Theme = {
+  fonts: {
+    body: 'Fira Sans'
+  },
+  colors: {
+    background: '#fff',
+    text: 'black'
+  },
+}
+
+export default theme
