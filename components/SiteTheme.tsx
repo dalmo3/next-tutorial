c6defdec90326f0c59701a4bf365c720c95278c6
Dalmo Mendonça <dalmo3@gmail.com>
2020-10-20 00:16:50 +1300

    more theme customisation

--- components/SiteTheme.tsx
+++ components/SiteTheme.tsx
@@ -32,6 +32,7 @@ const theme: Theme = merge(tailwind as Theme, {
   },
   styles: {
     inlineCode: {
+      color: 'black',
       fontFamily: 'code',
       fontSize: 'smaller',
       padding: '2px 5px',
@@ -73,14 +74,54 @@ const theme: Theme = merge(tailwind as Theme, {
     },
     h6: {
       fontFamily: 'headings'
-    }
-  },
-  sizes: {
-    container: ['100%', 600, 800, 1200]
-  },
-  links: {
-    nav: {
-      // fontSize: 99
+    },
+    // responsiveness
+    sizes: {
+      container: ['100%', 600, 800, 1200]
+    },
+    // imported from preset-bootstrap
+    img: {
+      maxWidth: '100%',
+      height: 'auto'
+    },
+    blockquote: {
+      fontSize: 2,
+      mb: 3,
+      '>p': {
+        display: 'flex',
+        backgroundColor: 'whitesmoke',
+        '&::before': {
+          background: 'lightgray',
+          padding: '0 1px',
+          margin: '0 10px 0 0',
+          content: `''`,
+          left: 0
+        }
+      }
+    },
+    table: {
+      width: '100%',
+      marginBottom: 3,
+      color: 'gray.9',
+      borderCollapse: 'collapse'
+    },
+    th: {
+      verticalAlign: 'bottom',
+      borderTopWidth: '2',
+      borderTopStyle: 'solid',
+      borderTopColor: 'gray.3',
+      borderBottomWidth: 2,
+      borderBottomStyle: 'solid',
+      borderBottomColor: 'gray.3',
+      padding: '.75rem',
+      textAlign: 'inherit'
+    },
+    td: {
+      borderBottomWidth: 2,
+      borderBottomStyle: 'solid',
+      borderBottomColor: 'gray.3',
+      verticalAlign: 'top',
+      padding: '.75rem'
     }
   }
 });
