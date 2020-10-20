3d4e56538a00719853797cd7246116b6ff7a9e37
Dalmo Mendonça <dalmo3@gmail.com>
2020-10-21 00:53:53 +1300

    attempt to close sidebar with overlay; fail

--- components/SiteSidebar.tsx
+++ components/SiteSidebar.tsx
@@ -1,6 +1,6 @@
 /** @jsx jsx */
 import { useContext } from 'react';
-import { Flex, Divider, jsx, Text } from 'theme-ui';
+import { Flex, Divider, jsx, Text, Box } from 'theme-ui';
 import Link from './Link';
 import { LayoutContext } from './SiteLayout';
 import { LocalStyles } from './SiteTheme';
@@ -8,14 +8,14 @@ import { LocalStyles } from './SiteTheme';
 const styles: LocalStyles = {
   container: {
     backgroundColor: 'whitesmoke',
-    flexDirection: 'column',
+    flexDirection: 'row',
     flex: '0 1 auto',
     //@ts-ignore
-    minWidth: (theme) => theme.sizes.sidebar,
-    py: 5,
-    px: 4
+    minWidth: (theme) => theme.sizes.sidebar
   },
   content: {
+    py: 5,
+    px: 4,
     position: 'fixed',
     flexDirection: 'column'
   },
@@ -40,6 +40,16 @@ const SiteSidebar = ({ sx = {} }) => {
               <Link href='/'>Contact</Link>
             </Flex>
           </Flex>
+          <Box
+            sx={{
+              bg: '#303030dd',
+              width: (theme) => `calc( 100vw - ${theme.sizes.sidebar}px )`,
+              right: 0,
+              position: 'absolute',
+              height: '10000%'
+            }}
+            onClick={toggleMenu}
+          />
         </Flex>
       )}
     </>
