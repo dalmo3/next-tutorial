e3bb640af6ee23f402cfa0e6848d8f03fa281bb3
Dalmo Mendonça <dalmo3@gmail.com>
2020-10-20 15:17:31 +1300

    footer

--- components/SiteFooter.tsx
+++ components/SiteFooter.tsx
@@ -1,5 +1,31 @@
-import { FC } from "react"
+import React, { FC } from 'react';
+import { Box, Flex, NavLink } from 'theme-ui';
+import Link from './Link';
 
-export const SiteFooter : FC = () => <div>site footer here</div>
+export const SiteFooter: FC = () => (
+  <footer>
+    <Flex
+      sx={{
+        flex: 'auto',
+        justifyContent: 'center',
+        backgroundColor: 'whitesmoke',
+        fontVariant: 'all-small-caps',
+        textAlign: 'center',
+        flexDirection: 'column',
+        lineHeight: 'initial'
+      }}
+    >
+      <Box>Made with 🤘 and Next.js</Box>
+      <Box>
+        <Link
+          href="https://github.com/dalmo3/next-tutorial"
+          navLinkProps={{ target: '_blank' }}
+        >
+          Source
+        </Link>
+      </Box>
+    </Flex>
+  </footer>
+);
 
-export default SiteFooter
+export default SiteFooter;
